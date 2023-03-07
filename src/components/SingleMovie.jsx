f830646c042228026e7124dd6c155c6784c334b0
dasha <dasha.danilchenko1990@gmail.com>
2023-03-07 21:30:11 +0200

    single genres

--- src/components/SingleMovie.jsx
+++ src/components/SingleMovie.jsx
@@ -11,20 +11,17 @@ const SingleMovie = ({idMovie}) => {
     .then(data => data.json())
     .then(movie => setMovie(movie))
   }, [idMovie])
-
   
   return (
     <div>
       <Link to=".." relative="path"><button>on the main</button></Link>
       <div>
-        <h2>{movie.title}</h2>
-        <img src={`${apiImg}${movie.poster_path}`} alt="poster" />
-        <p>genre:</p>
+        {movie.title && <h2>{movie.title}</h2>}
+        {movie.poster_path && <img src={`${apiImg}${movie.poster_path}`} alt="poster" />}
+        <p>genre: {movie.genres && movie.genres.map((i) => i.name).join(', ')}</p>
       </div>
     </div>
   )
 }
 
 export default SingleMovie
-
-{/* <p>genre:{`${movie.genres.map((i) => i.name).join(', ')}`}</p> */}
